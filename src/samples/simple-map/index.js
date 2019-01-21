14d7cac7d33cf22b1f08f26413c58b0bcba7372f
Rohan Dey <rohanoid5@gmail.com>
2019-01-21 16:42:45 +0530

    Fixed maps and special event samples

--- src/samples/simple-map/index.js
+++ src/samples/simple-map/index.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import FusionCharts from "fusioncharts/core";
-import Maps from "fusioncharts/maps";
-import World from "fusioncharts/maps/es/fusioncharts.world";
-import ReactFC from "react-fusioncharts";
-import FusionTheme from "fusioncharts/themes/es/fusioncharts.theme.fusion";
-import data from "./data.json";
+import React, { Component } from 'react';
+import FusionCharts from 'fusioncharts';
+import Maps from 'fusioncharts/fusioncharts.maps';
+import World from 'fusioncharts/maps/fusioncharts.world';
+import ReactFC from 'react-fusioncharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import data from './data.json';
 
 ReactFC.fcRoot(FusionCharts, Maps, World, FusionTheme);
 
 const chartConfigs = {
-  type: "world",
-  width: "100%",
-  height: "100%",
-  dataFormat: "json",
+  type: 'world',
+  width: '100%',
+  height: '100%',
+  dataFormat: 'json',
   dataSource: data
 };
 
